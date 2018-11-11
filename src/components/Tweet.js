a4032b323a655666d121c884a2877f4b77cdaa57
Ricardo Garcia <ricardoaugustogarcia@hotmail.com>
2018-11-10 22:56:54 -0200

    Ajuste no botão de Like no component Tweet

--- src/components/Tweet.js
+++ src/components/Tweet.js
@@ -7,6 +7,7 @@ import api from "../services/api";
 export default class Tweet extends Component {
   handleLike = async () => {
     const { _id } = this.props.tweet;
+    console.log("like?");
 
     await api.post(`/likes/${_id}`);
   };
@@ -18,7 +19,7 @@ export default class Tweet extends Component {
       <li className="tweet">
         <strong>{tweet.author}</strong>
         <p>{tweet.content}</p>
-        <button type="button" onClick={this.hadleLike}>
+        <button type="button" onClick={this.handleLike}>
           <img src={like} alt="like" />
           {tweet.likes}
         </button>
